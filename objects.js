41cc1a4187e498c7f1440896728c825c9c1ffaf1
MJU <marie.ju2012@outlook.com>
2020-03-10 19:20:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,8 @@
 // Assign an object to the variable playlist and
 // initialize the object with a key-value pair (artist name, song title)
 
+Object.assign({})
+
 function updatePlaylist(playlist, name, title) {
 
 }
