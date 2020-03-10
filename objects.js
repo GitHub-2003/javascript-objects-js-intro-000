d08bb1a20ededb37567cbcec6e6c2f8a28b969d6
MJU <marie.ju2012@outlook.com>
2020-03-10 19:43:48 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@
 // Assign an object to the variable playlist and
 // initialize the object with a key-value pair (artist name, song title)
 
-var playList = new Object({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'})
+var playlist = new Object({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'})
 
 // add the song and artist as a key-value pair to the playlist object.
 // return the whole playlist.
